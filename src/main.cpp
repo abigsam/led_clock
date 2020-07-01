2301d886f1c6224b0d16b0338f7f4bfa485476de
abigsam <abigsam@gmail.com>
2020-07-01 23:45:35 +0300

    Add main for display checking

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include "display.h"
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  display_pwr(0);
+  display_init();
+  display_set("1234");
+
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
